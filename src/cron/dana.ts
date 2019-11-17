784870a73e5f2c8ca4c05a8be8549104f6a78bd5
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-17 12:47:03 +0700

    edit cloudbuild yaml

--- src/cron/dana.ts
+++ src/cron/dana.ts
@@ -1,9 +1,6 @@
 import { CronJob } from 'cron'
 import { danaFood, danaGame, danaEntertainment } from '../functions/dana'
 import { Promo } from '../models'
-import Redis from 'ioredis'
-
-const redis = new Redis()
 
 new CronJob('* 0 0 * * *', async () => {
   try {
@@ -13,23 +10,6 @@ new CronJob('* 0 0 * * *', async () => {
       danaEntertainment()
     ])
     await Promise.all([Promo.insertMany([...foods, ...games, ...entertainments])])
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-new CronJob('* */15 * * *', async () => {
-  try {
-    const [foods, games, entertainments] = await Promise.all([
-      danaFood(),
-      danaGame(),
-      danaEntertainment()
-    ])
-    await Promise.all([
-      redis.set('dana-food', JSON.stringify(foods)),
-      redis.set('dana-games', JSON.stringify(games)),
-      redis.set('dana-entertainment', JSON.stringify(entertainments))
-    ])
   } catch (e) {
     console.log(e)
   }
